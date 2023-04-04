f3713de7c39b6dba838500174d5bb0bbec7bf6d8
7878bifiya <45232417+7878bifiya@users.noreply.github.com>
2023-04-05 00:15:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+from scipy.optimize import minimize_scalar
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 541133397 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+def solution(x: np.array) -> float:
+    mean = np.mean(checks)
+    ratio = mean / 371
+    omega_squared = np.log(1 + (np.std(checks)/mean)**2)
+    a = ratio * np.exp(omega_squared/2)
+    return a
